539608712d4297683617f2edd990a901ffb381e8
gusza110811 <100755292+gusza110811@users.noreply.github.com>
2024-02-01 21:40:28 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keyCipher',
-    version='0.1',
+    version='3',
     packages=find_packages(),
     description='A Python library for key-based encoding and decoding.',
     long_description=open('README.md').read() + ", no longer description available",
